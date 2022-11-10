51ed32fc6aaeae16c63eebf5a85fa47721926029
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-10 10:39:44 -0600

    feat(back): Add authentication

--- back/services/mongo.js
+++ back/services/mongo.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://127.0.0.1:27017/"
+
+// Replace the following with values for your environment.
+const username = encodeURIComponent("username");
+const password = encodeURIComponent("password");
+const clusterUrl = "127.0.0.1:27017";
+const authMechanism = "DEFAULT";
+
+const uri =
+  `mongodb://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
 
 const client = new MongoClient(uri);
 
@@ -9,7 +17,7 @@ async function connect() {
   try {
     await client.connect();
     console.log("Connected successfully to server");
-  } catch(error) {
+  } catch (error) {
     console.error(`[mongo:connect] Error: ${error.message}`);
   }
 }
