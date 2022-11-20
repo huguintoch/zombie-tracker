19c1ffaddaac1a85cb8b7ade3bd0369f9e0a893a
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 17:47:33 -0600

    feat(front): Add markers to map

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function fetchSightings() {
     var url = `http://${location.hostname}:3000/get-positions`;
-    fetch(url)
+    return fetch(url)
         .then(function (response) {
             return response.json();
         })
@@ -11,17 +11,20 @@ function fetchSightings() {
 
 function drawSightingsOnMap(sightings, map) {
     sightings.forEach(function (sighting) {
-        var marker = L.marker([sighting.latitude, sighting.longitude]).addTo(map);
-        marker.bindPopup(sighting.description);
+        var marker = L.marker([sighting.LAT, sighting.LON]).addTo(map);
+        marker.bindPopup(sighting.DESCRIPTION);
     });
 }
 
-window.addEventListener('DOMContentLoaded', function() { 
-    var map = L.map('map');   
+window.addEventListener('DOMContentLoaded', async function() {
+    var map = L.map('map').setView([20.574854, -103.440244], 10); 
+       
     L.tileLayer(`http://${location.hostname}:8080/tile/{z}/{x}/{y}.png`, {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    fetchSightings();
-    var sightings = JSON.parse(localStorage.getItem('sightings'));
-    drawSightingsOnMap(sightings, map);
+
+    fetchSightings().then(function () {
+        var sightings = JSON.parse(localStorage.getItem('sightings'));
+        drawSightingsOnMap(sightings, map);
+    });
 });
