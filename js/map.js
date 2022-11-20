ca564664530dc5f69995bb6b11d864ac0640bc8c
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 17:09:39 -0600

    feat(front): Init Leaflet script

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
-var coords = [20.574854, -103.440244]
-var map = L.map('map').setView(coords, 2);
-
-L.tileLayer('http://localhost:8080/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+window.addEventListener('DOMContentLoaded', function() {
+    var coords = [20.574854, -103.440244]
+    var map = L.map('map').setView(coords, 2);
+    
+    L.tileLayer('http://localhost:8080/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+});
