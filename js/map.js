e828cd0cc93d38c26a156669a5a2ea8f93232d64
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 17:13:30 -0600

    feat(front): Update map URL

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     var coords = [20.574854, -103.440244]
     var map = L.map('map').setView(coords, 2);
     
-    L.tileLayer(`http://${location.hostname}:8080/{z}/{x}/{y}.png`, {
+    L.tileLayer(`http://${location.hostname}:8080/tile/{z}/{x}/{y}.png`, {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
