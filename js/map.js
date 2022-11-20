906ad8040705c30374a96a5a69871b6d540aba24
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 17:58:54 -0600

    feat(front): Update marker colors

--- js/map.js
+++ js/map.js
@@ -18,14 +18,23 @@ function drawSightingsOnMap(sightings, map) {
 
     sightings.forEach(function (sighting) {
         var markerColor = colorTypeDict[sighting.TYPE];
-        var marker = L.circleMarker([sighting.LAT, sighting.LON], {radius: 10, fillColor: markerColor, fill: true, stroke: false}).addTo(map);
+        var marker = L.circleMarker(
+            [sighting.LAT, sighting.LON], 
+            { 
+                radius: 5, 
+                fillColor: markerColor,
+                fillOpacity: 1, 
+                fill: true, 
+                stroke: false 
+            }
+        ).addTo(map);
         marker.bindPopup(sighting.DESCRIPTION);
     });
 }
 
-window.addEventListener('DOMContentLoaded', async function() {
-    var map = L.map('map').setView([20.574854, -103.440244], 10); 
-       
+window.addEventListener('DOMContentLoaded', async function () {
+    var map = L.map('map').setView([20.574854, -103.440244], 10);
+
     L.tileLayer(`http://${location.hostname}:8080/tile/{z}/{x}/{y}.png`, {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
