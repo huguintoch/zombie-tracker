2b8ec96f15d80681efe3deb9e517c5065945a0d6
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 22:27:28 -0600

    feat(front): Update image caching method

--- sw.js
+++ sw.js
@@ -1,34 +1,40 @@
+const filesToCache = ['/js/leaflet.js', '/js/main.js', '/js/map.js', '/css/leaflet.css', '/css/nav-menu.css', '/css/style.css'];
+
 // This one stores an offline webpage in cache
-self.addEventListener("install", (e) => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll(["/offlineIndex.html", "/css/style.css", "/css/nav-menu.css"]);
+    caches.open('v1').then((cache) => {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
+// Take control of all pages under this SW's scope immediately, instead of waiting for reload/navigation.
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 // This one loads the saved webpage when the client is offline
-self.addEventListener("fetch", function (event) {
-  console.log('@fetch', event);
+self.addEventListener('fetch', function (event) {
   if (event.request.url.includes('/tile/')) {
     event.respondWith(
-      caches.open("v1").then(function (cache) {
+      caches.open('v1').then(function (cache) {
         return fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
           return response;
+        }).catch(function () {
+          return caches.match(event.request);
         });
       })
     );
   } else {
-    let cachedFileName = "";
-    if (event.request.url.includes("/css/nav-menu.css")) {
-      cachedFileName = "/css/nav-menu.css";
-    } else if (event.request.url.includes("/css/style.css")) {
-      cachedFileName = "/css/style.css";
-    } else {
-      cachedFileName = "/offlineIndex.html";
-    }
-
+    let cachedFileName = 'index.html';
+    filesToCache.some((file) => {
+      if (event.request.url.includes(file)) {
+        cachedFileName = file;
+        return true;
+      }
+    });
     event.respondWith(
       fetch(event.request).catch(function () {
         return caches.match(cachedFileName);
