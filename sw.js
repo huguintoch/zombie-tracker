ddf60a6a6879b04ba563e3352ddd22a2b86d2d7c
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 22:17:27 -0600

    feat(front): Update image caching method

--- sw.js
+++ sw.js
@@ -9,31 +9,29 @@ self.addEventListener("install", (e) => {
 
 // This one loads the saved webpage when the client is offline
 self.addEventListener("fetch", function (event) {
-  let cachedFileName = "";
-  if (event.request.url.includes("/css/nav-menu.css")) {
-    cachedFileName = "/css/nav-menu.css";
-  } else if (event.request.url.includes("/css/style.css")) {
-    cachedFileName = "/css/style.css";
-  } else {
-    cachedFileName = "/offlineIndex.html";
-  }
-
-  event.respondWith(
-    fetch(event.request).catch(function () {
-      return caches.match(cachedFileName);
-    })
-  );
-});
-
-// Cache tile images after requesting them
-self.addEventListener("fetch", function (event) {
-  if (event.request.url.includes("/tile/")) {
+  console.log('@fetch', event);
+  if (event.request.url.includes('/tile/')) {
     event.respondWith(
       caches.open("v1").then(function (cache) {
         return fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
           return response;
         });
+      })
+    );
+  } else {
+    let cachedFileName = "";
+    if (event.request.url.includes("/css/nav-menu.css")) {
+      cachedFileName = "/css/nav-menu.css";
+    } else if (event.request.url.includes("/css/style.css")) {
+      cachedFileName = "/css/style.css";
+    } else {
+      cachedFileName = "/offlineIndex.html";
+    }
+
+    event.respondWith(
+      fetch(event.request).catch(function () {
+        return caches.match(cachedFileName);
       })
     );
   }
