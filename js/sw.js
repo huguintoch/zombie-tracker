5d401f41552b242ea4ebc32a0b5a91b40b168cf2
Hugo Valdez <hugoisaac99@outlook.com>
2022-11-20 19:01:13 -0600

    Co-authored-by: Memohto <Memohto@users.noreply.github.com>

--- js/sw.js
+++ js/sw.js
@@ -3,37 +3,18 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('v1').then(function (cache) {
       return cache.addAll([
-        '/index.html',
+        '/offlineIndex.html',
         '/css/style.css',
-        '/css/leaflet.css',
         '/css/nav-menu.css',
-        '/js/main.js',
-        '/js/sw.js',
-        '/js/map.js',
-        '/js/leaflet.js',
       ]);
     })
   );
 });
 
-/* Add images to cache after fetching them */
-self.addEventListener('fetch', function (event) {
-  if (event.request.url.includes('tile')) {
-    event.respondWith(
-      caches.open('v1').then(function (cache) {
-        return fetch(event.request).then(function (response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      })
-    );
-  }
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match("offlineIndex.html");
+    })
+  );
 });
-
-// self.addEventListener("fetch", function(event) {
-//   event.respondWith(
-//     fetch(event.request).catch(function() {
-//       return caches.match(event.request.url);
-//     })
-//   );
-// });
